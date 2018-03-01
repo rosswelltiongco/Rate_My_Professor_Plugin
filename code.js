d0fd785e3a2f9cf269d46a5cecb13fe81edf8488
TheXulis <LFGonzalez1997@gmail.com>
2018-02-28 18:49:59 -0800

    Base functionality down, still contains a lot of bugs

--- code.js
+++ code.js
@@ -1,12 +1,25 @@
-console.log("tried");
 
-var i = 0;
-var id = "MTG_INSTR$"+i;
-var prof = document.getElementById(id);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    var i = 0;
+    var id = "MTG_INSTR$"+i;
+    var prof = document.getElementById(id);
+    var ids = JSON.parse(message);
 
-while(prof!=null){  
-    prof.innerHTML = "Professor #" + i++;
+    while(prof!=null){
 
-    id = "MTG_INSTR$"+i;
-    prof = document.getElementById(id);
-}
+        i++;
+
+        let profName = prof.innerHTML;
+
+        console.log(profName)
+
+        let profLink = "http://ratemyprofessors.com/ShowRatings.jsp?tid=" + ids[profName] + "&amp;showMyProfs=true";
+
+
+        prof.innerHTML = "<a class=\"PSLONGEDITBOX\" id=\"MTG_INSTR$0\" href= "+profLink+" rel=\"noopener noreferrer\" target=\"_blank\">"+profName+"</a>";
+
+        id = "MTG_INSTR$"+i;
+        prof = document.getElementById(id);
+    }
+});
+
--- content.js
+++ content.js
@@ -1,14 +1,31 @@
+
+
 const form = document.querySelector('form');
 form.addEventListener('submit', updateText);
 
 function updateText(e){
     e.preventDefault();
 
-    const find = document.querySelector('#find').value;
-    const replace = document.querySelector('#replace').value;
-    console.log(find + replace);
-
-    chrome.tabs.executeScript({
-        file: 'code.js'
+    var information = "";
+    
+    chrome.runtime.getPackageDirectoryEntry(function(root) {
+        root.getFile("ids.txt", {}, function(fileEntry) {
+          fileEntry.file(function(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(e) {
+                information = this.result;
+            };
+            reader.readAsText(file);
+          });
+        });
     });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabArr){
+        let id = tabArr[0].id;
+        chrome.tabs.executeScript(id, {file: 'code.js'}, function() {
+        chrome.tabs.sendMessage(id, information);
+    });
+    });
+
+    
 }
