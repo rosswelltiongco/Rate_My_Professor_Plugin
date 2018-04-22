860531edf675cd125eb14e7fd10c319ee4c5c88f
Andrew Soth <Andrew.Soths@gmail.com>
2018-04-22 13:09:20 -0700

    Added functionality to open professors tabs automatically, not yet separated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rate_My_Professor_Plugin
-Changing something
+
+This is a chrome extension developed for the students at CSULB. The extension only works within the class search on myCSULB.
--- code.js
+++ code.js
@@ -4,7 +4,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     var id = "MTG_INSTR$" + i;
     var prof = document.getElementById(id);
     var ids = JSON.parse(message);
-
+    var names = []
+    var unique = []
     var buttonId = "button_";
 
     while (prof != null) {
@@ -17,11 +18,27 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
         let profLink = "http://ratemyprofessors.com/ShowRatings.jsp?tid=" + ids[profName] + "&amp;showMyProfs=true";
 
+        if(profName != 'Staff'){ // only adds non Staff names links to the names array which contains duplicates
+            names.push(profLink);
+        }
 
-        prof.innerHTML = prof.innerHTML + "<br><button type='button' title=\"I am a tooltip!\"  id=\"" + buttonId + i + "\" onclick=\" window.open('" + profLink + "','_blank')\" >RMP</button><br>";
+         prof.innerHTML = prof.innerHTML + "<br><button type='button' title=\"I am a tooltip!\"  id=\"" + buttonId + i + "\" onclick=\" window.open('" + profLink + "','_blank')\" >RMP</button><br>";
 		//Fixme: look into 
         id = "MTG_INSTR$" + i;
         prof = document.getElementById(id);
     }
+
+
+
+    unique = names.filter(function(elem, index, self) { // removes duplicates from names array
+        return index === self.indexOf(elem);
+    })
+
+    for(var i=0; i<unique.length;i++){ // for each index in unique array, open a tab. (opens an error tab for proessors not in RMP ids.txt files) 
+        var redirectWindow = window.open(unique[i], '_blank');
+        redirectWindow.location;
+        console.log(unique[i]);
+    }
+    
 });
 
