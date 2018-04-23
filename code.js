ddd65782ebbf8ea947ddbb7396d18451fc863424
TheXulis <LFGonzalez1997@gmail.com>
2018-04-22 18:06:28 -0700

    Seperated createLinks function as well as adding information to local storage

--- code.js
+++ code.js
@@ -1,33 +1,19 @@
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    var i = 0;
-    var id = "MTG_INSTR$" + i;
-    var prof = document.getElementById(id);
-    var ids = JSON.parse(message);
-    var names = []
-    var unique = []
-    var buttonId = "button_";
 
-    while (prof != null) {
-
-        i++;
-
-        let profName = prof.innerHTML;
-
-        console.log(profName)
-
-        let profLink = "http://ratemyprofessors.com/ShowRatings.jsp?tid=" + ids[profName] + "&amp;showMyProfs=true";
-
-        if(profName != 'Staff'){ // only adds non Staff names links to the names array which contains duplicates
-            names.push(profLink);
-        }
-
-         prof.innerHTML = prof.innerHTML + "<br><button type='button' title=\"I am a tooltip!\"  id=\"" + buttonId + i + "\" onclick=\" window.open('" + profLink + "','_blank')\" >RMP</button><br>";
-		//Fixme: look into 
-        id = "MTG_INSTR$" + i;
-        prof = document.getElementById(id);
+    if(message.function == "createLinks"){
+        ids = JSON.parse(message.information);
+        createLinks(ids);
+        return;
     }
+    
+    var names = [];
+    var unique = [];
 
+    //Here is how you access the temporary memory to get the latest array of professor links
+    chrome.storage.sync.get('professors', function(result){
+        names = result.professors;
+    });
 
 
     unique = names.filter(function(elem, index, self) { // removes duplicates from names array
@@ -41,4 +27,36 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     }
     
 });
+
+function createLinks(ids){
+    var i = 0;
+    var id = "MTG_INSTR$" + i;
+    var prof = document.getElementById(id);
+    const buttonId = "button_";
+    var names = [];
+
+    while (prof != null) {
+
+        i++;
+
+        let profName = prof.innerHTML;
+
+        let profLink = "http://ratemyprofessors.com/ShowRatings.jsp?tid=" + ids[profName] + "&amp;showMyProfs=true";
+
+        if(profName != 'Staff'){ // only adds non Staff names links to the names array which contains duplicates
+            names.push(profLink);
+        }
+
+         prof.innerHTML = prof.innerHTML + "<br><button type='button' title=\"I am a tooltip!\"  id=\"" + buttonId + i + "\" onclick=\" window.open('" + profLink + "','_blank')\" >RMP</button><br>";
+		//Fixme: look into 
+        id = "MTG_INSTR$" + i;
+        prof = document.getElementById(id);
+    }
+
+    //Saving information to temporary memory
+    chrome.storage.sync.set({'professors': names}, function(){
+        console.log("set professors");
+    });
+    
+}
 
--- content.js
+++ content.js
@@ -6,12 +6,16 @@ function updateText(e) {
 
     var information = "";
 
+
+    
+
     chrome.runtime.getPackageDirectoryEntry(function (root) {
         root.getFile("ids.txt", {}, function (fileEntry) {
             fileEntry.file(function (file) {
                 var reader = new FileReader();
                 reader.onloadend = function (e) {
                     information = this.result;
+                    
                 };
                 reader.readAsText(file);
             });
@@ -21,7 +25,7 @@ function updateText(e) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabArr) {
         let id = tabArr[0].id;
         chrome.tabs.executeScript(id, { file: 'code.js' }, function () {
-            chrome.tabs.sendMessage(id, information);
+            chrome.tabs.sendMessage(id, {function: "createLinks", information: information});
         });
     });
 
