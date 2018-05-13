08165e856ad7e9d54aa194ed259dd0fd1cacdfe1
Andrew Soth <Andrew.Soths@gmail.com>
2018-05-12 18:40:01 -0700

    Added professor ratings color indicator into updateButtonTitle

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
 
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     
     if(message.function == "createLinks"){
@@ -32,6 +31,7 @@ function createLinks(ids){
     var prof = document.getElementById(id);
     const linkId = "link_";
     var names = [];
+    var profNames = [];
     var buttonId = "button_"
 
     if(prof.innerHTML.length > 30){
@@ -48,9 +48,17 @@ function createLinks(ids){
 
         let profLink = "http://ratemyprofessors.com/ShowRatings.jsp?tid=" + current_id + "&amp;showMyProfs=true";
 
-        if(profName != 'Staff'){ // only adds non Staff names links to the names array which contains duplicates
-            names.push(profLink);
-        }
+        // if(profName != 'Staff'){ // only adds non Staff names links to the names array which contains duplicates (used for openTabs function to filter array)
+        //     profNames.push(profName);
+        //     names.push(profLink);
+        // }
+
+        // if(profName === 'Staff'){ (helps to move on to next button to update if profName is Staff)
+        //     let current_button = buttonId + i;
+        //     id = "MTG_INSTR$" + i;
+        //     prof = document.getElementById(id);  
+        //     continue;
+        // }
 
         let current_button = buttonId + i       
 
@@ -70,6 +78,10 @@ function createLinks(ids){
         console.log("set professors");
     });
 
+    chrome.storage.sync.set({'professorNames': profNames},function(){
+        console.log("set professor names");
+    });
+
 }
 
  function scrapeRMP(){
@@ -78,6 +90,11 @@ function createLinks(ids){
     
     return scrapedInfo;
  }
+function clickButton(i){
+    var baseMultiplier = 2000;
+    var button = document.getElementById('button_'+i);
+    setTimeout(function(){button.click();},baseMultiplier*i);
+}
 
 function openTabs(){
 
@@ -104,7 +121,40 @@ function openTabs(){
         
 
     });
+    // var nameMemory = new Set();
+    // var i = 0;
+    // var id = "MTG_INSTR$" + i;
+    // var prof = document.getElementById(id);
+    // var buttonId = "button_"
+    
 
+    // while(prof != null){
+    //     var profName = prof.innerHTML;
+    //     i++;
+    //      if( (i+1) >= 10 ){
+    //           profName = prof.innerHTML.substring(172);
+    //      }
+    //      else{
+    //           profName = prof.innerHTML.substring(173);
+    //      }
+    //      console.log(profName);
+    //      if(nameMemory.has(profName) == false){
+    //         clickButton(i);
+    //         nameMemory.add(profName);
+    //      }
+    //     //Fixme: look into 
+    //     id = "MTG_INSTR$" + i;
+    //     prof = document.getElementById(id);
+    // }
+
+    // console.log(nameMemory);
+    // var currentButt = document.getElementById(buttond+i++);
+    // var nextButt = document.getElementById(buttond+3);
+    // console.log(id.innerHTML);
+    // currentButt.click();
+    // i = i+2;
+    // console.log('button' + i);
+    // setTimeout(function(){nextButt.click();},1500);
     
 
 }
@@ -145,7 +195,21 @@ function updateButtonTitle(prof_id, new_title){
         while(currentButton != null){
             if(currentButton.innerHTML == name){
                 currentButton.title = new_title;
-                currentButton.innerHTML = currentButton.innerHTML + " " + new_title.substring(17,20);
+                let grade = new_title.substring(17,20);
+                floatGrade = parseFloat(grade);
+                if(floatGrade >= 3.5){
+                    currentButton.innerHTML = currentButton.innerHTML + " " + "<span style = 'color: #0BB112;'><b>" +grade+ "</b></span>";
+                    
+                }
+                if(floatGrade >= 2.5 && grade < 3.5){
+                    currentButton.innerHTML = currentButton.innerHTML + " " + "<span style = 'color: #ECE505;'><b>" +grade+ "</b></span>";
+                    
+                }
+                if(floatGrade < 2.5){
+                    currentButton.innerHTML = currentButton.innerHTML + " " + "<span style = 'color: #CF3602;'><b>" +grade+ "</b></span>";
+                    
+                }
+               
             }
 
             currentButton = document.getElementById(buttonId+i++);
